37d45716615115cf6ead1fe813531412ee877b16
Imam Muis <imuis373@gmail.com>
2021-12-14 09:28:55 +0700

    begin

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 print('Hello World!')
 
 power = [i*i for i in range(1, 21)]
-print(power)
+# print(power)
+
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+    if i == 3:
+        break
+    else:
+        print("only executed when no item of the list is equal to 3")
